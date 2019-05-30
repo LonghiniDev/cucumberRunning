7eb019336d31d0ac8882c62eea852c4c96302bbf
LonghiniDev <guilherme.longhini@gmail.com>
2019-05-29 21:50:29 -0300

    Testes Java

--- Testes Java/src/testeRunners/cucumberRunning.java
+++ Testes Java/src/testeRunners/cucumberRunning.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features/teste.features")
+@CucumberOptions(features = "features/test1.feature")
 
 
 public class cucumberRunning {
