50f4e6c614ad0ee5477d5d85b895c19b5a8906f7
LonghiniDev <guilherme@gmail.com>
2019-05-29 21:31:36 -0300

    Vitão Monstrão

---
+++ Testes Java/src/testeRunners/cucumberRunning.java
@@ -0,0 +1,14 @@
+package testeRunners;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features/teste.features")
+
+
+public class cucumberRunning {
+
+}
